d7a7889057beee3667fc35a45883bae7afcbc177
Chelsea <hello@chelsea.lol>
2019-12-21 22:05:53 -0500

    Set style of clicked cell to brush style.

--- pixelArt.ts
+++ pixelArt.ts
@@ -1,38 +1,42 @@
 // Function to change the content of t2
-function modifyText() {
-	const t2 = document.getElementById("t2");
-	if (t2.firstChild.nodeValue == "three") {
-		t2.firstChild.nodeValue = "two";
-	} else {
-		t2.firstChild.nodeValue = "three";
-	}
+function applyBrush(e : Event)
+{
+	var targetElement = <HTMLElement>e.target;
+	targetElement.style.background = brush;
 }
 
-// Add event listener to table
-const el = document.getElementById("outside");
-el.addEventListener("click", modifyText, false);
+// Add event listener to cells
+const cells = document.getElementsByClassName("pattern_cell");
+for (var i = 0; i < cells.length; i++)
+{
+	cells[i].addEventListener("click", applyBrush, false);
+}
 
 // document.body.textContent = "Hallo Chelsea";
 
 // build brush
+var brush = "white";
 
 // hook handlers to pallet items
 function makeCurrentBlue() {
 	const currentBox = document.getElementById("current")
 	currentBox.firstChild.nodeValue = "blue"
 	currentBox.style.background = "blue";
+	brush = "blue";
 }
 
 function makeCurrentGreen() {
 	const currentBox = document.getElementById("current")
 	currentBox.firstChild.nodeValue = "green"
 	currentBox.style.background = "green";
+	brush = "green";
 }
 
 function makeCurrentPurple() {
 	const currentBox = document.getElementById("current")
 	currentBox.firstChild.nodeValue = "purple"
 	currentBox.style.background = "purple";
+	brush = "purple";
 }
 
 const element_blue = document.getElementById("blue");
