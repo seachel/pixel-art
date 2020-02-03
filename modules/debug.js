30081c71cc1747819c718baec91d6a4064d1986b
Chelsea <hello@chelsea.lol>
2020-02-03 10:41:47 -0500

    Added a module to help manage debugging.

---
+++ modules/debug.js
@@ -0,0 +1,11 @@
+export { _isDebug as isDebug, _writeDebug as writeDebug };
+export const _isDebug = true;
+export function _writeDebug(debugMessage, debugObject) {
+    console.log(`***
+	Debug note:
+	${debugMessage}
+	---
+	${debugObject}
+	***
+	`);
+}
